a61a402e5b60a4f291f8f3cdb9d8f3107d9f95a9
Shi Rao <vosaica@outlook.com>
2021-12-11 17:59:23 -0500

    Small changes

--- D3DApp_imgui/main.cpp
+++ D3DApp_imgui/main.cpp
@@ -244,7 +244,7 @@ void BoxApp::Draw(const Timer& gt)
 
     // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its
     // code to learn more about Dear ImGui!).
-    bool show_demo_window = true;
+    static bool show_demo_window = false;
     if (show_demo_window)
     {
         ImGui::ShowDemoWindow(&show_demo_window);
@@ -254,12 +254,13 @@ void BoxApp::Draw(const Timer& gt)
     {
         static int counter = 0;
 
-        ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+        ImGui::Begin("Box!"); // Create a window called "Hello, world!" and append into it.
 
         ImGui::Text("This is some useful text."); // Display some text (you can use a format strings too)
         ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
 
-        ImGui::SliderFloat("float", &mPhi, 0.1F, 1.0F); // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::SliderFloat("Phi", &mPhi, 0.001F, XM_PI - 0.001F);
+        ImGui::SliderFloat("Theta", &mTheta, -XM_2PI, XM_2PI);
 
         if (ImGui::Button("Button"))
         { // Buttons return true when clicked (most widgets return true when edited/activated)
